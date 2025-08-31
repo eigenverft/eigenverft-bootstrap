a9f6dc84dad184041025b61eb4f8f990068aea31
Eigenverft <eigenverft@outlook.com>
2025-08-31 11:46:56 +0200

    update

--- source/launcher.ps1
+++ source/launcher.ps1
@@ -21,9 +21,16 @@ if ($osName -notlike 'windows') {
 }
 
 
+try {
+    $r = Invoke-BuildIfRequired -SourceDir .\source\src -AssemblyName MyApp -ThisVersion 1.2.3.6 -OutPath .\out -Quiet
+    & $r.Executable.FullName
+}
+catch {
+    Out-Log @Logconfig -Level Error -Template "Error: @Messsage: {ExceptionMessage} @Script: {ExceptionScriptName} @Line: {ExceptionLineNumber} @At: {ExceptionLine}" -Params @{ ExceptionMessage = $_.Exception.Message; ExceptionScriptName = $_.InvocationInfo.ScriptName ; ExceptionLineNumber = $_.InvocationInfo.ScriptLineNumber ; ExceptionLine = $_.InvocationInfo.Line.Trim() }
+}
+
 # ---- resolve expected EXE path ----
-$r = Invoke-BuildIfRequired -SourceDir .\source\src -AssemblyName MyApp -ThisVersion 1.2.3.6 -OutPath .\out -Quiet
-& $r.Executable.FullName
+
 
 #$r = Set-AssemblyVersionAttributes -Path .\source\src -Version 1.2.3.4
 #$artifact = Invoke-CSharpCompilationEx -Source .\source\src -OutputType WindowsApplication -AssemblyName MyApp -OutputPath .\out -Configuration Release -PdbType None
