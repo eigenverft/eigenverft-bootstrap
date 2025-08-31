7722e032ba89e926f751656df4aa44972dbb5fad
Eigenverft <eigenverft@outlook.com>
2025-08-31 08:21:55 +0200

    launcher fix

---
+++ source/launcher.config.ps1
@@ -0,0 +1,23 @@
+$WriteLogInlineDefaultsProgressBar = @{
+    MinLevelPost    = 'Information'
+    MinLevelFile    = 'Error'
+    MinLevelConsole = 'Information'
+    ConsoleOverwriteLastLine = $true
+    ReturnJson    = $false
+    Endpoint   = 'https://localhost:8080/api/logs'
+    ApiKey     = 'your_api_key_here'
+    LogSpace   = 'foo'
+}
+
+$Logconfig = @{
+    MinLevelPost  = 'Information'
+    MinLevelFile  = 'Error'
+    MinLevelConsole = 'Verbose'
+    SuppressConsoleCaller = $false
+    ConsoleUseShortDate = $false
+    ConsoleOverwriteLastLine = $false
+    ReturnJson    = $false
+    Endpoint   = 'https://localhost:8080/api/logs'
+    ApiKey     = 'your_api_key_here'
+    LogSpace   = 'foo'
+}
--- source/launcher.ps1
+++ source/launcher.ps1
@@ -2,10 +2,16 @@
 . "$PSScriptRoot/scripts/Out-Log.ps1"
 . "$PSScriptRoot/scripts/Wait-PressKey.ps1"
 
+. "$PSScriptRoot/launcher.config.ps1"
+
+
 Clear-Host
 
+
+Out-Log @Logconfig -Level Information -Template "{Name} {State}." -Params @{ Name = 'eigenverft-bootstrappeer'; State = 'started' }
+
 Start-Sleep -Milliseconds 500
-Out-Log @WriteLogInlineDefaultsProgressBar -Level Information -Template "Script execution has started.."
+Out-Log @WriteLogInlineDefaultsProgressBar -Level Information -Template "Script execution has started.." -InitialWrite
 Start-Sleep -Milliseconds 500
 Out-Log  @WriteLogInlineDefaultsProgressBar -Level Information -Template "Script execution has started..."
 Start-Sleep -Milliseconds 500
